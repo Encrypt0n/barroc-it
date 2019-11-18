f53c70b97e22c2be426d2cbbe4d544f9e73a9090
Xander van Veen <xander.media.klas@gmail.com>
2019-11-18 14:37:45 +0100

    "Pakkende landingspagina" complete

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,8 +17,8 @@
 <body>
     <nav class="navbar navbar-expand-md navbar-light home-header">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+            <a class="navbar-brand header-logo-container" href="{{ url('/') }}">
+                <img class="header-logo" src="{{ asset('storage/img/Logo5_groot_cutout.png') }}" alt="logo5_groot">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
@@ -40,11 +40,6 @@
                         <li class="nav-item">
                             <a class="nav-link header-button" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
                     @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
